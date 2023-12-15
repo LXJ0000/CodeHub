7023055be0e5ebf9281033ec49d2ccf49aaa2051
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2023-12-15 15:23:14 +0800

    雪花算法实现分布式ID生成

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"bluebell/conf"
+	"bluebell/pkg/snowflake"
+)
 
+func main() {
+	conf.Init()
+	snowflake.Init(conf.Conf.StartTime, conf.Conf.MachineID)
+
+	//fmt.Println(snowflake.GenID())
 }
